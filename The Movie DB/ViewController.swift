bd31ac57951f2940d4f72a359f62c9e79cae7d43
Christian Vargas Saavedra <41171510+Lnk-Christian@users.noreply.github.com>
2021-08-03 21:25:54 -0500

    Initial Commit

---
+++ The Movie DB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  The Movie DB
+//
+//  Created by Christian Vargas Saavedra on 03/08/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
